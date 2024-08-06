7d1c09f1038494792f64ace334eefacee4c5c828
Anusha Sharma <aanya4sha@gmail.com>
2024-08-06 14:08:09 -0400

    Initial Commit

---
+++ aanyashair/aanyashairApp.swift
@@ -0,0 +1,17 @@
+//
+//  aanyashairApp.swift
+//  aanyashair
+//
+//  Created by aanya on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct aanyashairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
